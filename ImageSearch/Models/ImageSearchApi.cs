716bdcf22468f1c2ab1bb5d36a948bc6d45d3909
Casey Hudler <chudler624@gmail.com>
2022-09-28 13:24:15 -0400

    api page created

---
+++ ImageSearch/Models/ImageSearchApi.cs
@@ -0,0 +1,32 @@
+﻿namespace ImageSearch.Models
+{
+    public class ImageSearchApi
+    {
+
+        public ImageSearchApi() { }
+
+        public async Task GetImageAsync()
+        {
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "fe96c29976msh7164fa7f072bef1p19efc6jsn0ad83cbe2bb2" },
+		{ "X-RapidAPI-Host", "contextualwebsearch-websearch-v1.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				Console.WriteLine(body);
+			}
+
+		}
+
+
+    }
+}
